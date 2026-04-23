0481cbe42ad58959f5d4490461d5171e9aaaeeca
timofey.kars <timofey.kars@idfeurasia.pro>
2026-04-23 19:39:06 +0300

    update

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
-API_HOST = os.getenv("API_HOST", "http://127.0.0.1:8000")
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+API_HOST = os.getenv("API_HOST", "http://127.0.0.1:8000")
